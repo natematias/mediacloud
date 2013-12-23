a05c9f603681c83aaf47168b1e29ec18f90ed531
Ubuntu <ubuntu@mediacloud.local>
2013-12-23 18:57:21 +0000

    Add skeleton code for the extractor service.

---
+++ lib/MediaWords/Controller/Api/Extractor/V1/Extractor.pm
@@ -0,0 +1,79 @@
+package MediaWords::Controller::Api::Extractor::V1::Extractor;
+use Modern::Perl "2013";
+use MediaWords::CommonLibs;
+
+use MediaWords::DBI::StorySubsets;
+
+use strict;
+use warnings;
+use base 'Catalyst::Controller';
+use JSON;
+use List::Util qw(first max maxstr min minstr reduce shuffle sum);
+use Moose;
+use namespace::autoclean;
+use List::Compare;
+use Carp;
+
+=head1 NAME
+
+MediaWords::Controller::Stories - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+=head2 index 
+
+=cut
+
+BEGIN { extends 'Catalyst::Controller::REST' }
+
+__PACKAGE__->config(
+    'default'   => 'application/json',
+    'stash_key' => 'rest',
+    'map'       => {
+
+        #	   'text/html'          => 'YAML::HTML',
+        'text/xml' => 'XML::Simple',
+
+        # #         'text/x-yaml'        => 'YAML',
+        'application/json'         => 'JSON',
+        'text/x-json'              => 'JSON',
+        'text/x-data-dumper'       => [ 'Data::Serializer', 'Data::Dumper' ],
+        'text/x-data-denter'       => [ 'Data::Serializer', 'Data::Denter' ],
+        'text/x-data-taxi'         => [ 'Data::Serializer', 'Data::Taxi' ],
+        'application/x-storable'   => [ 'Data::Serializer', 'Storable' ],
+        'application/x-freezethaw' => [ 'Data::Serializer', 'FreezeThaw' ],
+        'text/x-config-general'    => [ 'Data::Serializer', 'Config::General' ],
+        'text/x-php-serialization' => [ 'Data::Serializer', 'PHP::Serialization' ],
+    },
+);
+
+__PACKAGE__->config( json_options => { relaxed => 1, pretty => 1, space_before => 1, space_after => 1 } );
+
+sub extract : Local : ActionClass('REST')
+{
+}
+
+sub extract_PUT : Local
+{
+    my ( $self, $c ) = @_;
+    my $extract = $c->req->data;
+
+}
+
+sub extract_GET : Local
+{
+    my ( $self, $c, $id ) = @_;
+    my $extract = $c->req->data;
+
+    my $story_extract = $c->dbis->find_by_id( 'story_extracts', $id );
+
+}
+
+
+1;
