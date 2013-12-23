6d91cfb310e36df184a3c5ee016f8aa95e225bc1
natematias <natematias@gmail.com>
2013-12-23 16:39:51 -0500

    updated script to use extractor_hostname properly

--- extractor-examples/query_extractor.rb
+++ extractor-examples/query_extractor.rb
@@ -7,14 +7,14 @@ require 'json'
 #download an article from the Telegraph
 
 uri = ARGV[0] || "http://www.bbc.co.uk/newsbeat/20714096"
-extractor_hostname = ARGV[1] || ""
+extractor_hostname = ARGV[1] || "ec2-23-22-34-227.compute-1.amazonaws.com"
 port = ARGV[2] || "3000"
 
 #parse the title
 
 html = Net::HTTP.get(URI(uri))
 
-http = Net::HTTP.new("ec2-23-22-34-227.compute-1.amazonaws.com", port)
+http = Net::HTTP.new(extractor_hostname, port)
 request = Net::HTTP::Put.new("/api/extractor/v1/extractor/extract")
 request.set_form_data({"title"=>"Call Me Maybe named as the most watched video of 2012", "doc"=>html})
 response = http.request(request)
